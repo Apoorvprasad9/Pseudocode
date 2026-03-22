577ad8e27c71afc3ad074cc5c9bd3a960910fd3d
Apoorvprasad9 <apoorvgupta958@gmail.com>
2026-03-22 23:47:22 +0530

    update code

--- funnoutput4.java
+++ funnoutput4.java
@@ -1,16 +1,16 @@
 //pseudocode
 /*
 Integer funn(Integer a,Integer b,Integer c)
-       b=(a^11)+b
+       b=(a^11)&b
        for(each c from 3 to 6)
                b=3+c
-               if((1>b) || (a&c) ||(c-a))
+               if((1<b) || (a&c) <(c-a))
                     a(c+2)+c
                      b=c+c
                      jump out of the loop
                 end if
          end for
-return a+b+c
+return a+b
 End function
 */
 //java code
@@ -29,15 +29,15 @@ public class funnoutput4 {
     }
 
     public static int funn(int a, int b, int c) {
-        b = (a ^ 11) + b;
+        b = (a ^ 11) & b;
         for (c = 3; c <= 6; c++) {
             b = 3 + c;
-            if ((1 > b) || (a & c) != 0 || (c - a) != 0) {
+            if ((1 < b) || (a & c) < (c - a)) {
                 a = (c + 2) + c;
                 b = c + c;
                 break; // jump out of the loop
             }
         }
-        return a + b + c;
+        return a + b;
     }
 }
